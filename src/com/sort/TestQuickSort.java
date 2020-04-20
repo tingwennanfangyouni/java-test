9b6b07ea8d3b7558cdbc19268404a8a13fbb3231
tingwennanfangyouni <642559390@qq.com>
2020-04-20 16:47:50 +0800

    添加了注释

--- src/com/sort/TestQuickSort.java
+++ src/com/sort/TestQuickSort.java
@@ -12,7 +12,10 @@ public class TestQuickSort {
 
     /**
      *
-     * @param arr
+     * @param arr 快速排序是一种排序执行效率很高的排序算法，它利用分治法来对待排序序列进行分治排序，
+     *            它的思想主要是通过一趟排序将待排记录分隔成独立的两部分，其中的一部分比关键字小，
+     *            后面一部分比关键字大，然后再对这前后的两部分分别采用这种方式进行排序，
+     *            通过递归的运算最终达到整个序列有序
      * @param start
      * @param end
      */
@@ -22,10 +25,18 @@ public class TestQuickSort {
             int high = end;
             int standard = arr[low];
             while(low < high){
-                while(low < high && standard < arr[high])high--;
-                if(low < high) arr[low++] = arr[high];
-                while(low < high && standard > arr[low])low++;
-                if(low < high) arr[high--] = arr[low];
+                while(low < high && standard < arr[high]) {
+                    high--;
+                }
+                if(low < high){
+                    arr[low++] = arr[high];
+                }
+                while(low < high && standard > arr[low]){
+                    low++;
+                }
+                if(low < high) {
+                    arr[high--] = arr[low];
+                }
             }
             arr[low] = standard;
             QuickSort(arr, start, low - 1);
