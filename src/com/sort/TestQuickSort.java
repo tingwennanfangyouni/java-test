9e90a719960d30a22a57d8283111da2137a697b1
tingwennanfangyouni <642559390@qq.com>
2020-04-28 15:35:27 +0800

    排序的类

--- src/com/sort/TestQuickSort.java
+++ src/com/sort/TestQuickSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class TestQuickSort {
     public static void main(String[] args) {
-        int[] arr2 = new int[] {1,2,3};
+        int[] arr2 = new int[] {1,2,3,2,0,9};
         System.out.println("排序前的数组："+Arrays.toString(arr2));
         QuickSort(arr2, 0, arr2.length - 1);
         System.out.println("排序后的数组："+Arrays.toString(arr2));
@@ -19,28 +19,21 @@ public class TestQuickSort {
      * @param start
      * @param end
      */
-    public static void QuickSort(int[] arr,int start, int end){
-        if(start < end){
-            int low = start;
-            int high = end;
-            int standard = arr[low];
-            while(low < high){
-                while(low < high && standard < arr[high]) {
-                    high--;
-                }
-                if(low < high){
-                    arr[low++] = arr[high];
-                }
-                while(low < high && standard > arr[low]){
-                    low++;
-                }
-                if(low < high) {
-                    arr[high--] = arr[low];
-                }
+    public static void QuickSort(int[] arr,int start, int end)
+    {
+        if (start<end){
+            int low=start;
+            int high=end;
+            int standard=arr[low];
+            while (low<high){
+                while(low<high&&standard<arr[high]) high--;
+                if(low<high) arr[low++]=arr[high];
+                while (low<high&&standard>arr[low]) low++;
+                if(low<high) arr[high--]=arr[low];
             }
-            arr[low] = standard;
-            QuickSort(arr, start, low - 1);
-            QuickSort(arr, low + 1, end);
+            arr[low]=standard;
+            QuickSort(arr,start,low-1);
+            QuickSort(arr,low+1,end);
         }
     }
 }
